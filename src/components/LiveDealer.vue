545a293dedb45c5a91a34f4924562815d0f8d2d1
joseph <mmx362003@gmail.com>
2023-02-10 01:30:59 +0800

    Add the Style

--- src/components/LiveDealer.vue
+++ src/components/LiveDealer.vue
@@ -1,45 +1,112 @@
 <template>
-  <div class="flex justify-center w-screen">
-    <div class="mt-[100px]">
+  <div
+    class="w-screen flex justify-center items-center flex-wrap"
+    v-on:click="movePosition($event)"
+  >
+    <div
+      class="mt-[100px] w-full flex justify-center items-center cursor-pointer"
+    >
       <div
-        class="flex w-[960px] h-[80px] flex-row justify-center border-green-600"
+        class="flex font-bold w-[960px] h-[80px] flex-row justify-center shadow-lg shadow-green-900"
       >
         <div
-          class="flex-grow first:border-l-2 w-1/3 last:border-r-2 border-t-2 border-b-2 border-green-600"
+          class="flex-grow w-1/3 flex justify-center items-center text-white gap-2 bg-green-800 hover:bg-green-400"
         >
-          <span
-            class="inline-block w-full h-full align-middle text-center p-[20px]"
-            >BP</span
-          >
+          BP
         </div>
         <div
-          class="flex-grow first:border-l-2 w-1/3 last:border-r-2 border-t-2 border-b-2 border-green-600"
+          class="flex-grow w-1/3 flex justify-center items-center text-white gap-2 bg-green-800 hover:bg-green-400"
         >
-          <span
-            class="inline-block w-full h-fullalign-middle text-center p-[20px]"
-            >BP</span
-          >
+          P
         </div>
         <div
-          class="flex-grow first:border-l-2 w-1/3 last:border-r-2 border-t-2 border-b-2 border-green-600"
+          class="flex-grow w-1/3 flex justify-center items-center text-white gap-2 bg-green-800 hover:bg-green-400"
         >
-          <span
-            class="inline-block w-full h-full align-middle text-center p-[20px]"
-            >BP</span
-          >
+          PP
         </div>
       </div>
-      <div class="bg-green-400">Place Bet</div>
+    </div>
+
+    <div
+      class="mt-[250px] w-full flex justify-center items-center cursor-pointer"
+    >
+      <div
+        class="flex w-[960px] h-[100px] flex-row shadow-lg shadow-green-900 font-bold bg-green-800"
+      >
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          10
+        </div>
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          10
+        </div>
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          20
+        </div>
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          30
+        </div>
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          40
+        </div>
+        <div
+          class="rounded-full border-2 h-[80px] border-white text-white w-[80px] p-5 flex justify-center items-center ml-3 hover:bg-green-400"
+        >
+          50
+        </div>
+      </div>
     </div>
   </div>
+  <div
+    class="w-[50px] h-[50px] absolute clickCircle"
+    v-bind:style="{ top: yPosition, left: xPosition }"
+  ></div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const xPosition = ref('0px');
+const yPosition = ref('0px');
+function movePosition(event: any) {
+  xPosition.value = (event.pageX - 25).toString() + 'px';
+  yPosition.value = (event.pageY - 25).toString() + 'px';
+}
+</script>
 
 <style scoped>
-.flex-item {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.clickCircle:active {
+  box-sizing: border-box;
+  border-style: solid;
+  border-color: black;
+  background-color: transparent;
+  border-radius: 50%;
+  animation: clickEffect 0.4s ease-out;
+  z-index: 99999;
+}
+
+@keyframes clickEffect {
+  0% {
+    opacity: 1;
+    width: 1em;
+    height: 1em;
+    margin-top: 25px;
+    margin-left: 25px;
+    border-width: 0.5em;
+  }
+  100% {
+    opacity: 0.2;
+    width: 5em;
+    height: 5em;
+    border-width: 0.03em;
+  }
 }
 </style>
